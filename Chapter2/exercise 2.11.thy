56e811735e1ae3c3eae71ef336e811c97d6b000e
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-02 01:10:11 -0700

    new coeffs, multCoeffsHelper

--- Chapter2/exercise 2.11.thy
+++ Chapter2/exercise 2.11.thy
@@ -38,17 +38,18 @@ value "createPolyExpression [4,2,-1] 0"
   (* Define a function evalPoly that evaluates a polynomial at the given value. *)
   (* Evaluate a polynomial, given its coefficients and x. The coefficients are assumed to start at
   x^0, be in order, and contiguous *)
-fun evalPoly :: "int list \<Rightarrow> int \<Rightarrow> int" where
+(* fun evalPoly :: "int list \<Rightarrow> int \<Rightarrow> int" where
   "evalPoly coeffs v = eval (createPolyExpression coeffs 0) v"
   
 value "evalPoly [4,2,-1,3] 1"  
-value "evalPoly [4,2,-1,3] 3"
+value "evalPoly [4,2,-1,3] 3" *)
   
   (* Takes the reversed coefficient list. I.e. [-1, 2, 4] represents 4+2x-x^2   *)
 fun hornerHelper :: "int list \<Rightarrow> int \<Rightarrow> int" where
   "hornerHelper [] _ = 0"|
   "hornerHelper (rc # rcs) v = rc * v^(length rcs) + (hornerHelper rcs v)"
   
+  (* Evaluate a polynomial (using Horner's method) *)
 fun evalPolyHorner :: "int list \<Rightarrow> int \<Rightarrow> int" where
   "evalPolyHorner coeffs v = hornerHelper (rev coeffs) v"
   
@@ -137,7 +138,7 @@ fun simplifyNonrecursive :: "exp \<Rightarrow> exp" where
   
   
   (* declare [[ smt_timeout = 30 ]] *)
-    
+  
 lemma simplifyNonrecursive_preserves_eval[simp]: "eval (simplifyNonrecursive expr) x = eval expr x"
   apply(induction expr rule: simplifyNonrecursive.induct) apply(auto)
   done 
@@ -165,8 +166,8 @@ value "simplify (Add (Mult (Const 4) (Const 1)) (Add (Mult (Add (Const 0) (Const
 lemma simplify_preserves: "eval (simplify expr) x = eval expr x"
   apply(induction expr rule: simplify.induct) apply(auto) 
   done    
-
-  (* Define a function coeffs that transforms an expression into a polynomial. I'm going to limit my
+    
+    (* Define a function coeffs that transforms an expression into a polynomial. I'm going to limit my
 function to handling canonical expressions only. I assume 0 coefficients are included, 
 e.g. (Mult (Const 0) Var) *)
     (* TODO/done sort the adds by number of nested multiplies *)
@@ -180,6 +181,42 @@ fun coeffsOld :: "exp \<Rightarrow> int list" where
   
 value "coeffsOld (createPolyExpression [4,2,-1] 0)"
 value "coeffsOld (createPolyExpression [4,2,-1,3] 0)"
+  
+fun addCoeffs :: "int list \<Rightarrow> int list \<Rightarrow> int list" where
+  "addCoeffs [] r = r" |
+  "addCoeffs l [] = l" |
+  "addCoeffs (l#ls) (r#rs) = (l+r) # addCoeffs ls rs"
+  
+lemma 
+  "evalPolyHorner(coeffs expr) x = eval expr x"
+         hornerHelper (rev (addCoeffs (coeffs expr1) (coeffs expr2))) x = eval expr1 x + eval expr2 x
+
+  
+  (* left, right, prefix (a list of some 0s)   *)
+fun multCoeffsHelper :: "int list \<Rightarrow> int list \<Rightarrow> int list \<Rightarrow> int list" where
+  "multCoeffsHelper [] _  _ = []" |
+  "multCoeffsHelper _  [] _ = []" |
+  "multCoeffsHelper (l#ls) rs prefix = 
+    (let this = prefix @ (map (\<lambda>ri. l*ri) rs);
+          next = multCoeffsHelper ls rs (0 # prefix)
+      in addCoeffs this next)"
+  
+fun multCoeffs :: "int list \<Rightarrow> int list \<Rightarrow> int list" where  
+  "multCoeffs l r = multCoeffsHelper l r []"
+  
+fun coeffs :: "exp \<Rightarrow> int list" where
+  "coeffs Var = [0, 1]"  |
+  "coeffs (Const i) = [i]"|
+  "coeffs (Add l r) = addCoeffs (coeffs l) (coeffs r)" |
+  "coeffs (Mult l r) = multCoeffs (coeffs l) (coeffs r)"
+  
+value "coeffs (createPolyExpression [4,2,-1] 0)"
+value "coeffs (Mult (createPolyExpression [1,2,3] 0) (createPolyExpression [4,5] 0))"
+  
+theorem ceoffs_preserves_eval[simp]: "evalPolyHorner(coeffs expr) x = eval expr x"
+  apply(induction expr)
+     apply(auto)
+  done
   
   (* 
 I have not been able to complete the proof portion of exercise 2.11.
