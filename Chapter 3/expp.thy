c91e83f0a6a0a687833d428b5ebf425f9320ae24
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-08 00:39:34 -0700

    delete broken proofs

--- Chapter 3/expp.thy
+++ Chapter 3/expp.thy
@@ -66,33 +66,7 @@ fun plus :: "expr \<Rightarrow> expr \<Rightarrow> expr" where
   "plus (N i) a = (if i=0 then a else Plus (N i) a)" |
   "plus a (N i) = (if i=0 then a else Plus a (N i))" |
   "plus a\<^sub>1 a\<^sub>2 = Plus a\<^sub>1 a\<^sub>2"
- 
-lemma eval_plus[simp]:
-  "fst (eval (plus a1 a2) s) = fst (eval a1 s) + fst (eval a2 s)"
-  (* apply(induction a1 a2 rule: plus.induct) Original. The 'a1 a2' is extraneous. *)
-  apply(induction rule: plus.induct)
-    apply simp
-    apply simp
-    apply simp
-    apply simp
-  apply (simp add: prod.case_eq_if)
-    apply simp
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-  apply (simp add: prod.case_eq_if)
-                      (* apply auto *)
-    (* apply (simp_all add: prod.case_eq_if) *)
-              apply simp_all (* just for a change from auto *)
-  done
-    
+     
 fun times :: "expr \<Rightarrow> expr \<Rightarrow> expr" where
   "times (N i\<^sub>1) (N i\<^sub>2) = N(i\<^sub>1 * i\<^sub>2)" |
   "times (N i) a = (if i=0 
@@ -106,60 +80,14 @@ fun times :: "expr \<Rightarrow> expr \<Rightarrow> expr" where
                       then a
                       else Times a (N i)))" |
   "times a\<^sub>1 a\<^sub>2 = Times a\<^sub>1 a\<^sub>2"
-  
-lemma eval_times[simp]:
-  "eval (times a1 a2) s = eval a1 s * eval a2 s"
-  (* apply(induction a1 a2 rule: times.induct) Original. The 'a1 a2' is extraneous. *)
-  apply(induction rule: times.induct)
-    apply simp_all
-  done  
-  
+
 fun asimp :: "expr \<Rightarrow> expr" where
-  "asimp (N n) = N n" |
-  "asimp (V x) = V x" |
   "asimp (Plus a\<^sub>1 a\<^sub>2) = plus (asimp a\<^sub>1) (asimp a\<^sub>2)" |
-  "asimp (Times a\<^sub>1 a\<^sub>2) = times (asimp a\<^sub>1) (asimp a\<^sub>2)"
+  "asimp (Times a\<^sub>1 a\<^sub>2) = times (asimp a\<^sub>1) (asimp a\<^sub>2)"|
+  (* Don't recurse inside N, V, or PostIncr *)
+  "asimp e = e"
  
 value "asimp (Plus (Plus (N 0) (N 0)) (Plus (V ''x'') (N 0)))"
- 
-theorem eval_asimp[simp]:
-  "eval (asimp a) s = eval a s"
-  apply(induction a)
-  apply simp_all
-  done
- 
-(* Define a substitution function
-subst :: vname \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr
-such that
-subst x a e
-is the result of replacing every
-occurrence of variable x by a in e *)
-fun subst :: "vname \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr" where
-  "subst matchMe replaceWith (V vname) =
-      (if matchMe = vname
-      then replaceWith
-      else V vname)"|
-  "subst matchMe replaceWith (N n) = N n"|
-  "subst matchMe replaceWith (Plus e\<^sub>1 e\<^sub>2) =
-      Plus (subst matchMe replaceWith e\<^sub>1) (subst matchMe replaceWith e\<^sub>2)"|
-  "subst matchMe replaceWith (Times e\<^sub>1 e\<^sub>2) =
-      Times (subst matchMe replaceWith e\<^sub>1) (subst matchMe replaceWith e\<^sub>2)"
- 
-value "subst ''x'' (N 3) (Plus (V ''x'') (V ''y'')) = Plus (N 3) (V ''y'')"
- 
-(*   Prove the so-called substitution lemma that says that we can either
-substitute first and evaluate afterwards or evaluate with an updated state:
-eval (subst x a e) s = eval e (s(x := eval a s)). As a consequence prove
-eval a1 s = eval a2 s =\<Rightarrow> eval (subst x a1 e) s = eval (subst x a2 e) s. *)
-lemma substitution:"eval (subst x a e) s = eval e (s(x := eval a s))"
-  apply(induction e)
-  by auto
- 
-theorem foo:
-  "eval a\<^sub>1 s = eval a\<^sub>2 s
-  \<Longrightarrow> eval (subst x a\<^sub>1 e) s = eval (subst x a\<^sub>2 e) s"
-  apply(induction e)
-  by auto
- 
+
 end
   
