2ea0f3b96cac91111e26fd14dce2ce06a1579cc4
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-08 00:40:38 -0700

    begin ex 3.5

---
+++ Chapter 3/expp.thy
@@ -0,0 +1,147 @@
+section "Arithmetic and Boolean Expressions"
+ 
+theory expp (* short for for expression ++ *)
+  imports Main "~~/src/HOL/Library/Code_Target_Nat" 
+begin
+  
+  (* exercise 3.5
+impl post-increment
+division operation *)
+ 
+subsection "Arithmetic Expressions"
+ 
+type_synonym vname = string
+type_synonym val = int
+type_synonym state = "vname \<Rightarrow> val"
+ 
+datatype expr = N int | V vname | PostIncr vname | Plus expr expr | Times expr expr
+  
+(* Returns the updated state *)
+fun increment_variable :: "vname \<Rightarrow> state \<Rightarrow> state" where
+  "increment_variable v s = 
+    (let before = s v
+        in s (v := before + 1))"
+ 
+fun eval :: "expr \<Rightarrow> state \<Rightarrow> val \<times> state" where
+  "eval (N n) s = (n, s)" |
+  "eval (V v) s = (s v, s)" |
+  "eval (PostIncr v) s = (s v, increment_variable v s)" |
+  "eval (Plus a\<^sub>1 a\<^sub>2) s = 
+    (let (r\<^sub>1, s\<^sub>1) = eval a\<^sub>1 s;
+         (r\<^sub>2, s\<^sub>2) = eval a\<^sub>2 s\<^sub>1
+      in (r\<^sub>1 + r\<^sub>2, s\<^sub>2))"|
+  "eval (Times a\<^sub>1 a\<^sub>2) s = 
+    (let (r\<^sub>1, s\<^sub>1) = eval a\<^sub>1 s;
+         (r\<^sub>2, s\<^sub>2) = eval a\<^sub>2 s\<^sub>1
+      in (r\<^sub>1 * r\<^sub>2, s\<^sub>2))"
+  
+value "eval (Plus (V ''x'') (N 5)) (\<lambda>x. if x = ''x'' then 7 else 0)"
+ 
+value "eval (Plus (V ''x'') (N 5)) ((\<lambda>x. 0) (''x'':= 7))"
+ 
+definition null_state ("<>") where
+  "null_state \<equiv> \<lambda>x. 0"
+syntax
+  "_State" :: "updbinds => 'a" ("<_>")
+translations
+  "_State ms" == "_Update <> ms"
+  "_State (_updbinds b bs)" <= "_Update (_State b) bs"
+ 
+lemma "<a := 1, b := 2> = (<> (a := 1)) (b := (2::int))"
+  by (rule refl)
+ 
+value "eval (Plus (V ''x'') (N 5)) <''x'' := 7>"
+ 
+value "eval (Plus (V ''x'') (N 5)) <''y'' := 7>"
+  
+value "eval (Plus (V ''x'') (V ''x'')) <''x'' := 1>"
+
+  (* test post increment *)
+value "eval (Plus (PostIncr ''x'') (PostIncr ''x'')) <''x'' := 1>"
+
+subsection "Constant Folding"
+ 
+fun plus :: "expr \<Rightarrow> expr \<Rightarrow> expr" where
+  "plus (N i\<^sub>1) (N i\<^sub>2) = N(i\<^sub>1+i\<^sub>2)" |
+  "plus (N i) a = (if i=0 then a else Plus (N i) a)" |
+  "plus a (N i) = (if i=0 then a else Plus a (N i))" |
+  "plus a\<^sub>1 a\<^sub>2 = Plus a\<^sub>1 a\<^sub>2"
+ 
+lemma eval_plus[simp]:
+  "eval (plus a1 a2) s = eval a1 s + eval a2 s"
+  (* apply(induction a1 a2 rule: plus.induct) Original. The 'a1 a2' is extraneous. *)
+  apply(induction rule: plus.induct)
+    (* apply(induction a1) apply(induction a2)  Does not work*)
+              apply simp_all (* just for a change from auto *)
+  done
+    
+fun times :: "expr \<Rightarrow> expr \<Rightarrow> expr" where
+  "times (N i\<^sub>1) (N i\<^sub>2) = N(i\<^sub>1 * i\<^sub>2)" |
+  "times (N i) a = (if i=0 
+                    then (N 0) 
+                    else (if i=1 
+                      then a
+                      else Times (N i) a))" |
+  "times a (N i) = (if i=0 
+                    then (N 0) 
+                    else (if i=1 
+                      then a
+                      else Times a (N i)))" |
+  "times a\<^sub>1 a\<^sub>2 = Times a\<^sub>1 a\<^sub>2"
+  
+lemma eval_times[simp]:
+  "eval (times a1 a2) s = eval a1 s * eval a2 s"
+  (* apply(induction a1 a2 rule: times.induct) Original. The 'a1 a2' is extraneous. *)
+  apply(induction rule: times.induct)
+    apply simp_all
+  done  
+  
+fun asimp :: "expr \<Rightarrow> expr" where
+  "asimp (N n) = N n" |
+  "asimp (V x) = V x" |
+  "asimp (Plus a\<^sub>1 a\<^sub>2) = plus (asimp a\<^sub>1) (asimp a\<^sub>2)" |
+  "asimp (Times a\<^sub>1 a\<^sub>2) = times (asimp a\<^sub>1) (asimp a\<^sub>2)"
+ 
+value "asimp (Plus (Plus (N 0) (N 0)) (Plus (V ''x'') (N 0)))"
+ 
+theorem eval_asimp[simp]:
+  "eval (asimp a) s = eval a s"
+  apply(induction a)
+  apply simp_all
+  done
+ 
+(* Define a substitution function
+subst :: vname \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr
+such that
+subst x a e
+is the result of replacing every
+occurrence of variable x by a in e *)
+fun subst :: "vname \<Rightarrow> expr \<Rightarrow> expr \<Rightarrow> expr" where
+  "subst matchMe replaceWith (V vname) =
+      (if matchMe = vname
+      then replaceWith
+      else V vname)"|
+  "subst matchMe replaceWith (N n) = N n"|
+  "subst matchMe replaceWith (Plus e\<^sub>1 e\<^sub>2) =
+      Plus (subst matchMe replaceWith e\<^sub>1) (subst matchMe replaceWith e\<^sub>2)"|
+  "subst matchMe replaceWith (Times e\<^sub>1 e\<^sub>2) =
+      Times (subst matchMe replaceWith e\<^sub>1) (subst matchMe replaceWith e\<^sub>2)"
+ 
+value "subst ''x'' (N 3) (Plus (V ''x'') (V ''y'')) = Plus (N 3) (V ''y'')"
+ 
+(*   Prove the so-called substitution lemma that says that we can either
+substitute first and evaluate afterwards or evaluate with an updated state:
+eval (subst x a e) s = eval e (s(x := eval a s)). As a consequence prove
+eval a1 s = eval a2 s =\<Rightarrow> eval (subst x a1 e) s = eval (subst x a2 e) s. *)
+lemma substitution:"eval (subst x a e) s = eval e (s(x := eval a s))"
+  apply(induction e)
+  by auto
+ 
+theorem foo:
+  "eval a\<^sub>1 s = eval a\<^sub>2 s
+  \<Longrightarrow> eval (subst x a\<^sub>1 e) s = eval (subst x a\<^sub>2 e) s"
+  apply(induction e)
+  by auto
+ 
+end
+  
