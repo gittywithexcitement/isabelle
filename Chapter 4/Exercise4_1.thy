73ac10d8d38b463557698b7e4e247876f002144f
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-26 01:22:36 -0700

    define insert and one theorem

--- Chapter 4/Exercise4_1.thy
+++ Chapter 4/Exercise4_1.thy
@@ -4,21 +4,20 @@ theory Exercise4_1
 begin
   
 section "Exercise 4.1"
-
+  
 datatype 'a tree = Tip | Node "'a tree" 'a "'a tree"
   
 fun to_set :: "'a tree \<Rightarrow> 'a set" where
   "to_set Tip = {}" |
   "to_set (Node lc x rc) = {x} \<union> to_set lc \<union> to_set rc"
-
+  
 value "to_set Tip"
 value "to_set (Node Tip 0 (Node (Node Tip 2 Tip) 1 Tip)) :: int set"
   
-(* A tree is ordered if:
+  (* A tree is ordered iff:
 at a node, the value is > all values in the left child
 and < all values in the right child 
-and both of the node's children are ordered*)
-  
+and both of the node's children are ordered*)  
 fun ord :: "int tree \<Rightarrow> bool" where
   "ord Tip = True" |
   "ord (Node lc x rc) = (
@@ -29,5 +28,29 @@ fun ord :: "int tree \<Rightarrow> bool" where
 value "ord (Node (Node Tip 2 Tip) 3 Tip)"
 value "ord (Node Tip 0 (Node (Node Tip 1 Tip) 2 Tip))"
   
+  (* Insert element into ordered int tree, preserving order   *)
+fun ins :: "int \<Rightarrow> int tree \<Rightarrow> int tree" where
+  "ins x Tip = Node Tip x Tip" |  
+  "ins x (Node lc tx rc) = (
+    if x < tx
+    then Node (ins x lc) tx rc
+    else (
+      if x > tx
+      then Node lc tx (ins x rc)
+      else (Node lc tx rc)))"  
   
+value "ins 1 (Node (Node Tip 2 Tip) 4 Tip)"
+value "ins 2 (Node (Node Tip 2 Tip) 4 Tip)"
+value "ins 3 (Node (Node Tip 2 Tip) 4 Tip)"
+value "ins 5 (Node (Node Tip 2 Tip) 4 Tip)"
+
+value "ord (ins 1 (Node (Node Tip 2 Tip) 4 Tip))"
+value "ord (ins 2 (Node (Node Tip 2 Tip) 4 Tip))"
+value "ord (ins 3 (Node (Node Tip 2 Tip) 4 Tip))"
+value "ord (ins 5 (Node (Node Tip 2 Tip) 4 Tip))"
+  
+theorem ins_tree_like_insert_set:"to_set (ins x tree) = {x} \<union> to_set tree"
+  apply(induction tree)
+  by auto
+    
 end
