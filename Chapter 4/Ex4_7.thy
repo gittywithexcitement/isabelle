c3e31c9a79d03a7406753a8c91e9549e1e3f06c4
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-30 01:26:24 -0700

    fix add, I think

--- Chapter 4/Ex4_7.thy
+++ Chapter 4/Ex4_7.thy
@@ -7,13 +7,9 @@ section "Exercise 4.6"
 inductive ok :: "nat \<Rightarrow> instr list \<Rightarrow> nat \<Rightarrow> bool" where
   empty: "ok n [] n" |
   loadi: "ok _ is n \<Longrightarrow> ok _ ((LOADI _) # is) (n + 1)" |
-  (* literal: "ok nb is na \<Longrightarrow> ok n ((LOADI _) # is) (na + 1)" *)
   load: "ok _ is n \<Longrightarrow> ok _ ((LOAD _) # is) (n + 1)" |
-  add: "ok _ is n \<Longrightarrow> n \<ge> 2 \<Longrightarrow> ok _ (ADD # is) (n - 2)"
-  (* add: "ok _ is _ \<Longrightarrow> n \<ge> 2 \<Longrightarrow> ok n (ADD # is) (n - 2)" *)
-  
-  (* datatype instr = LOADI val | LOAD vname | ADD *)
-  
+  add: "ok _ is n \<Longrightarrow> n\<^sub>a\<^sub>t\<^sub>l\<^sub>e\<^sub>a\<^sub>s\<^sub>t \<ge> 2 \<Longrightarrow> ok n\<^sub>a\<^sub>t\<^sub>l\<^sub>e\<^sub>a\<^sub>s\<^sub>t (ADD # is) (n - 2)"
+
 lemma test0_1:"ok 0 [LOADI 9] 1"
   using empty loadi by fastforce
     
@@ -30,4 +26,6 @@ lemma test1_3:"ok 1 [LOADI 9, LOADI 9] 3"
   using loadi 
   by (metis add.commute add_One add_One_commute empty inc.simps(2) nat_1_add_1 one_plus_numeral)    
 
+    (* TODO test add *)
+    
 end
