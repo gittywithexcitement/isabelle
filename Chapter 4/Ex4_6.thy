ee673ae9c5b9a715bccb1e79f2bf7087c9807b33
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-29 22:25:56 -0700

    finish exercise: prove equivalence

--- Chapter 4/Ex4_6.thy
+++ Chapter 4/Ex4_6.thy
@@ -19,5 +19,37 @@ proof -
   moreover have "aval_rel (N 4) <> 4" using literal by simp
   ultimately show "aval_rel (Plus (N 2) (N 4)) <> 6" using plus by fastforce
 qed
+  
+lemma aval_rel_implies_aval:"aval_rel exp state v \<Longrightarrow> aval exp state = v"
+  apply(induction rule: aval_rel.induct)
+  by simp_all
+    
+lemma aval_implies_aval_rel:"aval exp state = v \<Longrightarrow> aval_rel exp state v"
+  (* This is a great example of 'arbitrary' being required.
+If you do induction without arbitrary v, then the third case, plus, can't be solved. The inductive
+hypotheses are too weak; if you used them you would essentially be saying: (v + v = v)
+The two premises of the induction hypothesis each say that the evaluation of their own
+expression equals some v; we need 'arbitrary' to indicate that these v may be different from each
+other, and different from the v in the proposition we are proving.
+This is what the goal looks like without aribtrary:
 
+ 3. \<And>exp1 exp2.
+       (aval exp1 state = v \<Longrightarrow> Ex4_6.aval_rel exp1 state v) \<Longrightarrow>
+       (aval exp2 state = v \<Longrightarrow> Ex4_6.aval_rel exp2 state v) \<Longrightarrow> 
+        aval (Plus exp1 exp2) state = v 
+    \<Longrightarrow> Ex4_6.aval_rel (Plus exp1 exp2) state v
+
+Obviously, the 'v' in the first two lines should be different from the v in the last line, hence
+'arbitrary' is required. *)
+  apply(induction exp arbitrary: v)
+    apply (simp add: literal)
+  using variable apply auto[1]
+  using plus by auto
+    
+lemma "aval exp state = v \<longleftrightarrow> aval_rel exp state v"
+  apply rule
+   apply (simp add: aval_implies_aval_rel)
+  by (simp add: aval_rel_implies_aval)
+    
+    
 end
