db0b3445e1b7c2ecf3a0b3a7e85f32b01340067d
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-30 01:22:46 -0700

    fixed error in inductive prop; lemmas work now

--- Chapter 4/Ex4_7.thy
+++ Chapter 4/Ex4_7.thy
@@ -6,39 +6,28 @@ section "Exercise 4.6"
   
 inductive ok :: "nat \<Rightarrow> instr list \<Rightarrow> nat \<Rightarrow> bool" where
   empty: "ok n [] n" |
-  loadi: "ok _ is n \<Longrightarrow> ok n ((LOADI _) # is) (n + 1)" |
+  loadi: "ok _ is n \<Longrightarrow> ok _ ((LOADI _) # is) (n + 1)" |
   (* literal: "ok nb is na \<Longrightarrow> ok n ((LOADI _) # is) (na + 1)" *)
-  load: "ok _ is n \<Longrightarrow> ok n ((LOAD _) # is) (n + 1)" |
-  add: "ok _ is n \<Longrightarrow> n \<ge> 2 \<Longrightarrow> ok n (ADD # is) (n - 2)"
+  load: "ok _ is n \<Longrightarrow> ok _ ((LOAD _) # is) (n + 1)" |
+  add: "ok _ is n \<Longrightarrow> n \<ge> 2 \<Longrightarrow> ok _ (ADD # is) (n - 2)"
   (* add: "ok _ is _ \<Longrightarrow> n \<ge> 2 \<Longrightarrow> ok n (ADD # is) (n - 2)" *)
   
   (* datatype instr = LOADI val | LOAD vname | ADD *)
   
-lemma test0:"ok 0 [LOADI 9] 1"
+lemma test0_1:"ok 0 [LOADI 9] 1"
   using empty loadi by fastforce
-
+    
 lemma "ok 1 [LOADI 9] 2"
   using empty loadi by (metis nat_1_add_1)
-
+    
 lemma "ok n [LOADI 9] (n+1)"
   using empty loadi by blast 
-
-lemma test1:"ok 0 [LOADI 9, LOADI 9] 2"
-  (* using loadi test0 sledgehammer *)
-proof -
-  have "ok 0 [LOADI 9] 1" using test0 by simp
-  then have "ok 0 [LOADI 9, LOADI 9] (1+1)" 
-    using loadi 
-    nitpick
-      
-
-(* lemma "aval_rel (Plus (N 2) (N 4)) <> 6"
-proof -
-  have "aval_rel (N 2) <> 2" using literal by simp
-  moreover have "aval_rel (N 4) <> 4" using literal by simp
-  ultimately show "aval_rel (Plus (N 2) (N 4)) <> 6" using plus by fastforce
-qed
- *)  
+    
+lemma test0_2:"ok 0 [LOADI 9, LOADI 9] 2"
+  using test0_1 loadi by (metis one_add_one)
   
-  
+lemma test1_3:"ok 1 [LOADI 9, LOADI 9] 3"
+  using loadi 
+  by (metis add.commute add_One add_One_commute empty inc.simps(2) nat_1_add_1 one_plus_numeral)    
+
 end
