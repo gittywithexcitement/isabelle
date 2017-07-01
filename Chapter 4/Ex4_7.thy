6798de652c03ab5d6a5d3dbf05701dd3f91a0b6f
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-06-30 21:24:51 -0700

    test add

--- Chapter 4/Ex4_7.thy
+++ Chapter 4/Ex4_7.thy
@@ -9,7 +9,7 @@ inductive ok :: "nat \<Rightarrow> instr list \<Rightarrow> nat \<Rightarrow> bo
   loadi: "ok _ is n \<Longrightarrow> ok _ ((LOADI _) # is) (n + 1)" |
   load: "ok _ is n \<Longrightarrow> ok _ ((LOAD _) # is) (n + 1)" |
   add: "ok _ is n \<Longrightarrow> n\<^sub>a\<^sub>t\<^sub>l\<^sub>e\<^sub>a\<^sub>s\<^sub>t \<ge> 2 \<Longrightarrow> ok n\<^sub>a\<^sub>t\<^sub>l\<^sub>e\<^sub>a\<^sub>s\<^sub>t (ADD # is) (n - 2)"
-
+  
 lemma test0_1:"ok 0 [LOADI 9] 1"
   using empty loadi by fastforce
     
@@ -21,11 +21,28 @@ lemma "ok n [LOADI 9] (n+1)"
     
 lemma test0_2:"ok 0 [LOADI 9, LOADI 9] 2"
   using test0_1 loadi by (metis one_add_one)
-  
+    
 lemma test1_3:"ok 1 [LOADI 9, LOADI 9] 3"
   using loadi 
   by (metis add.commute add_One add_One_commute empty inc.simps(2) nat_1_add_1 one_plus_numeral)    
-
-    (* TODO test add *)
-    
+        
+lemma "ok 2 [ADD] 0"
+proof -
+  have "ok 2 [] 2" 
+    using empty by metis
+  hence "ok 2 [ADD] (2-2)" using add by blast
+  thus ?thesis by simp 
+qed
+  
+lemma "ok 2 [ADD, LOADI 1] 1"
+proof -
+  have "ok 2 [] 2" 
+    using empty by metis
+  hence "ok 2 [ADD] 0" using add by fastforce
+  hence "ok 2 [ADD, LOADI 1] (0+1)"
+    using loadi by (metis add.left_neutral add_diff_cancel_left' ok.simps order_refl)
+ thus ?thesis
+   by simp
+qed
+  
 end
