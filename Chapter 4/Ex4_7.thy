bd8c2413be4d622446cff35f8c20da2852e39c36
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-07-02 00:59:27 -0700

    WIP ok_computes_stack_size

--- Chapter 4/Ex4_7.thy
+++ Chapter 4/Ex4_7.thy
@@ -14,12 +14,38 @@ theorem ok_computes_stack_size:
   "\<lbrakk>ok n instructions n'; length stack = n \<rbrakk> \<Longrightarrow> length (exec instructions state stack) = n'"
 proof(induction rule: ok.induct) (* arbitrary n? stack?*)
   case (empty n)
+  then show ?case by simp
+next
+  case (loadi n\<^sub>b instrs n\<^sub>a val)
+    apply_end(rename_tac n\<^sub>b instrs n\<^sub>a val)
+
+  have "exec (LOADI val # instrs) state stack = exec instrs state (exec1 (LOADI val) state stack)"
+    by simp
+  also have "... = exec instrs state (val # stack)" by simp
+
+  then show ?case try0 sledgehammer sorry
+next
+  case (load n\<^sub>b "is" n\<^sub>a uv)
+  then show ?case (* try0 sledgehammer *) sorry
+next
+  case (add n\<^sub>b "is" n\<^sub>a)
+  then show ?case (* try0 sledgehammer *) sorry
+qed 
+  
+(*case (empty  n)
   then show ?case 
     by simp
 next
-  (* case (loadi len0 instructions len1 uv uw) *)
-  case (loadi uu "is" n uv uw)
-    apply_end(rename_tac len0 instrctns len1 uv uw)
+  (* case (loadi lenStk instrs lenInstrsRslt l val) *)
+ case (loadi uu "is" n uv uw)
+   apply_end(rename_tac lenStk instrs lenInstrsRslt l val)
+    (* case_names rename_tac len0 instrctns len1 uv uw *)
+  have "exec (LOADI val # instrs) state stack = exec instrs state (exec1 (LOADI val) state stack)"
+    by simp
+    (* n # stk *)
+  also have "... = exec instrs state (val # stack)" by simp
+    
+    
     (*     fix len0 instrs len1 lngthstck val
     let "?case" = "length (exec (LOADI val # instrs) state stack) = len1 + 1"
     assume hyps : "ok len0 instrs len1" 
@@ -33,13 +59,8 @@ next
   then show ?case (* sledgehammer *) sorry
 next
   case (add va instructions n n\<^sub>a\<^sub>t\<^sub>l\<^sub>e\<^sub>a\<^sub>s\<^sub>t)
-  then show ?case sledgehammer sorry
-qed
-  (*   apply(induction rule: ok.induct)
-     apply(simp)
-    apply(simp)
- *)    
-  oops  
+  then show ?case (* sledgehammer *) sorry
+qed  *)
     
 lemma test0_1:"ok 0 [LOADI 9] 1"
   using empty loadi by fastforce
