96a2e87c89be5d315cc0a8dabf8a0a872c7f6064
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-07-03 20:22:10 -0700

    do 5.2

--- Chapter 5/Whole.thy
+++ Chapter 5/Whole.thy
@@ -10,6 +10,7 @@ lemma
     and axy:"A x y" (* Should this be prefixed with \<forall> x y? *)
   shows "T x y" (* Should this be prefixed with \<forall> x y? *)
 proof cases
+  (* NB: the positive case has to come first, negative case comes second *)
   assume "A x y \<and> A y x"
   hence "x = y" using A by blast
   thus "T x y"
@@ -22,4 +23,29 @@ next
   ultimately show "T x y" by metis
 qed
 
+lemma "(\<exists> ys zs. xs = ys @ zs \<and> length ys = length zs) 
+     \<or> (\<exists> ys zs. xs = ys @ zs \<and> length ys = length zs + 1)"
+proof cases
+  assume "even (length xs)"
+  then obtain halfLength where hl:"2 * halfLength = length xs" 
+    by (metis evenE)
+  then obtain ys\<^sub>p where y:"ys\<^sub>p = take halfLength xs" by simp 
+  then obtain zs\<^sub>p where z:"zs\<^sub>p = drop halfLength xs" by simp
+  have "ys\<^sub>p @ zs\<^sub>p = xs"
+    by (simp add: y z)
+  moreover have "length ys\<^sub>p = length zs\<^sub>p" using hl y z by fastforce
+  ultimately show ?thesis by fastforce 
+next
+  assume "\<not>even (length xs)"
+  hence "odd (length xs)" by simp
+  then obtain halfLength where hl:"2 * halfLength + 1 = length xs"
+    by (metis oddE) 
+  then obtain ys\<^sub>p where y:"ys\<^sub>p = take (halfLength + 1) xs" by simp 
+  then obtain zs\<^sub>p where z:"zs\<^sub>p = drop (halfLength + 1) xs" by simp
+  have "ys\<^sub>p @ zs\<^sub>p = xs"
+    by (simp add: y z)
+  moreover have "length ys\<^sub>p = length zs\<^sub>p + 1" using hl y z by fastforce
+  ultimately show ?thesis by fastforce 
+qed 
+  
 end
