2cb0259ca5f734e8d77243eb928cb8fcf4f69f31
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-07-04 23:22:45 -0700

    Exercise 5.5

--- Chapter 5/Whole.thy
+++ Chapter 5/Whole.thy
@@ -121,6 +121,26 @@ proof
   qed
 qed
   
+subsection "Exercise 5.5"
+  
+inductive star :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" for r where
+  refl: "star r x x" |
+  step: "r x y \<Longrightarrow> star r y z \<Longrightarrow> star r x z"
+  
+inductive iter :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> nat \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" for r where
+  refl: "iter r 0 x x" |
+  (* will the order of the premises make proving harder? *)
+  (* step: "iter r n x y \<Longrightarrow> r y z \<Longrightarrow> iter r (Suc n) x z" *)
+  step: "r x y \<Longrightarrow> iter r n y z \<Longrightarrow> iter r (Suc n) x z"
+  
+lemma "iter r n x y \<Longrightarrow> star r x y"
+proof(induction rule: iter.induct)
+  case (refl x)
+  then show ?case using star.refl by fast
+next
+  case (step x y n z)
+  then show ?case by (meson star.step)
+qed
   
   
 end
