de4877c3066a8a6a78e132aa7ce295dabcdae960
Daniel Killebrew <killebrew.daniel@gmail.com>
2017-07-22 01:29:42 -0700

    found an error. Fraction y > 0 is false

--- work/Denorm.thy
+++ work/Denorm.thy
@@ -52,6 +52,12 @@ proof(rule ccontr)
   assume "\<not>(\<nexists>x. x < SmallPositiveDenorm \<and> x > Plus_zero \<and> Finite x)"
   hence 0:"(\<exists>x. x < SmallPositiveDenorm \<and> x > Plus_zero  \<and> Finite x)" by auto
   then obtain y where y:"y < SmallPositiveDenorm \<and> y > Plus_zero \<and> Finite y" by auto
+  obtain ys ye yf where ysef:"(ys, ye, yf) = Rep_float y" 
+    by (metis Abs_float_inverse finite_nan float_double_neg_eq float_neg_def fneg_def 
+        is_valid_defloat mem_Collect_eq neg_valid y)
+  hence fesy:"Sign y = ys \<and> Exponent y = ye \<and> Fraction y = yf"
+    by (metis Exponent_def Fraction_def Sign_def exponent.simps fraction.simps sign.simps)
+
   have "Sign y < 2"
     using Sign_def is_valid_defloat is_valid by auto
   hence 1:"Sign y = 0 \<or> Sign y = 1"
@@ -69,6 +75,61 @@ proof(rule ccontr)
     then show ?thesis 
       using Finite_def float_le_neg float_zero1 y by blast 
   qed
+    
+  have "Val y > 0"
+    using Finite_def Val_zero float_zero1 y by auto
+  hence valof:"valof float_format (0, ye, yf) > 0"
+    using Val_def fesy ypos ysef by auto
+
+  have "Fraction y > 0"
+  proof(rule ccontr)
+    assume "\<not>(Fraction y > 0)"
+    hence "Fraction y \<le> 0" by simp
+    hence "yf \<le> 0"
+      by (simp add: fesy)
+    hence "yf = 0" by simp 
+    hence "valof float_format (0, ye, 0) > 0"      
+      using valof by auto
+        
+    have "\<forall>exp. valof float_format (0, exp, 0) = 0"
+    proof
+      fix exp
+      have "valof float_format (0, exp, 0) = 0"
+      proof(cases "exp = 0")
+        case True
+        then show ?thesis 
+          by simp 
+      next
+        case False
+          (* Crap, I forgot about the implicit 1 *)
+        hence "valof float_format (0, exp, 0) = ((2^exp) / (2^bias float_format)) * (1 + 0)"
+          by simp
+        then show ?thesis try
+      qed
+        
+      show ?thesis
+        proof
+      have "valof float_format (0, exp, 0) = (-1::real)^0 * (2 / (2^bias float_format)) * (real 0/2^(fracwidth float_format))"
+      (* have "valof float_format (0, exp, 0) = 1 * (2 / (2^bias float_format)) * (real 0)" *)
+        sledgehammer
+          sorry
+    qed
+(*     hence "valof float_format (0, ye, yf) > 0"
+      try *)
+    then show False 
+      sledgehammer sorry
+  qed
+    
+  have "Exponent y = 0"
+  proof(rule ccontr)
+    assume "Exponent y \<noteq> 0"
+    (* hence 0:"(\<exists>x. x < SmallPositiveDenorm \<and> x > Plus_zero  \<and> Finite x)" by auto *)
+    then show False sorry
+  qed
+
+    (* using 0 *)
+    (* try *)
+    
     
 
   then show False sorry
