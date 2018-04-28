b308c11931adad397d4a18ee19d63a1ef8db93cf
Daniel Killebrew <killebrew.daniel@gmail.com>
2018-04-28 12:51:46 -0700

    define unique

---
+++ theorem-prover-showdown/unique.thy
@@ -0,0 +1,227 @@
+theory unique
+  imports Main   
+  "~~/src/HOL/Library/Code_Target_Nat"
+  (* "~~/src/HOL/Library/Fset" *)
+  "~~/src/HOL/Library/Finite_Set"
+begin
+
+section \<open>define unique\<close>
+  
+(* unique. Takes a sequence of integers, returns the unique elements of that
+list. There is no requirement on the ordering of the returned values. 
+
+inspired by https://www.hillelwayne.com/post/theorem-prover-showdown/ *)
+
+fun uniqueAccum :: "nat list \<Rightarrow> nat list \<Rightarrow> nat list" where
+  "uniqueAccum [] accum = accum" |
+  "uniqueAccum (x # xs) accum = uniqueAccum xs (List.insert x accum)"
+(* note that List.insert uses set *) 
+
+fun unique :: "nat list => nat list" where
+  "unique xs = uniqueAccum xs []"
+
+(* Prove: 
+
+All elements of the original list are in the output
+
+Every element of the output is distinct
+
+Note from webpage author:
+
+I specified that all elements of the original list are in the output, but not
+that all elements of the output were in the original list. If the method took
+in [1, 2, 2] and returned [1, 2, 99], it would still pass the partial
+specification. I really dropped the ball on this one. *)
+
+section \<open>Proofs\<close>
+
+subsection \<open>All elements of the original list are elements of the output\<close>
+
+lemma all_elements_present:"\<forall> x. x \<in> set xs \<Longrightarrow> x \<in> set (unique xs)"
+proof(induction xs)
+  case Nil
+  then show ?case 
+    by simp
+next
+  case (Cons x xs)
+  then show ?case 
+    apply auto
+    try
+    sorry
+qed
+
+text "additional characters are padding"
+lemma right_pad_adds_padding_character:
+  fixes lst :: "'a list"
+    and p :: "'a"
+    and padTo :: nat
+  assumes "length lst < padTo"
+    and "length lst + n = padTo"
+  shows "\<lbrakk>length lst < padTo; length lst + n = padTo\<rbrakk> 
+      \<Longrightarrow> drop (length lst) (rightPad p lst padTo) = replicate n p"
+proof(induction lst arbitrary: padTo)
+  case Nil
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case by simp
+  next
+    case (Suc padTo)
+    then show ?case 
+      by (simp add: rightpad_empty_is_replicate)
+  qed
+next
+  case (Cons l ls)
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case by simp
+  next
+    case (Suc padT)
+    then show ?case by simp
+  qed
+qed
+
+text "prefix of result is the list"
+lemma right_pad_prefix_is_list:
+  fixes lst :: "'a list"
+    and p :: "'a"
+    and padTo :: nat
+  shows "take (length lst) (rightPad p lst padTo) = lst"
+proof(induction lst arbitrary: padTo)
+  case Nil
+  then show ?case 
+    by simp
+next
+  case (Cons a lst)
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case 
+      by simp
+  next
+    case (Suc padTo)
+    then show ?case 
+      by simp
+  qed
+qed
+
+text "length is correct"
+lemma right_pad_length_is_correct:
+  shows "length (rightPad p lst padTo) = max (length lst) padTo"
+proof(induction lst arbitrary: padTo)
+  case Nil
+  then show ?case 
+    by (simp add: rightpad_empty_is_replicate)
+next
+  case (Cons a lst)
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case 
+      by simp
+  next
+    case (Suc padTo)
+    then show ?case 
+      by simp
+  qed
+qed
+
+subsection \<open>left pad\<close> 
+
+text "length is correct"
+theorem left_pad_length_is_correct:
+  shows "length (leftPad p lst padTo) = max (length lst) padTo"
+  by (simp add: right_pad_length_is_correct)
+
+text "suffix of result is the list"
+theorem left_pad_suffix_is_list:
+  fixes lst :: "'a list"
+    and p :: "'a"
+    and padTo :: nat
+  assumes "length lst < padTo"
+    and "length lst + n = padTo"
+  shows "\<lbrakk>length lst + n = padTo\<rbrakk> \<Longrightarrow> drop n (leftPad p lst padTo) = lst"
+proof(induction lst)
+  case Nil
+  then show ?case 
+    by (simp add: rightpad_empty_is_replicate) 
+next
+  case (Cons l ls)
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case 
+      by simp 
+  next
+    case (Suc padTo\<^sub>p)
+    have "drop n (leftPad p ls padTo\<^sub>p) = ls" 
+      apply auto
+    proof -
+      have "length ls \<le> padTo\<^sub>p"
+        by (metis (no_types) Suc.prems(2) Suc_inject add.commute add_Suc_right le_add1 length_Cons)
+      then have "length (rightPad p (rev ls) padTo\<^sub>p) = padTo\<^sub>p"
+        by (simp add: right_pad_length_is_correct)
+      then have "length (rightPad p (rev ls) padTo\<^sub>p) - n = length ls"
+        by (metis (no_types) Suc.prems(2) add.commute add_Suc_right add_diff_cancel_left' length_Cons)
+      then show "drop n (rev (rightPad p (rev ls) padTo\<^sub>p)) = ls"
+        by (metis (no_types) drop_rev length_rev rev_rev_ident right_pad_prefix_is_list)
+    qed
+    then show ?case
+      apply auto
+      proof -
+        have "length (rightPad p (rev ls @ [l]) (Suc padTo\<^sub>p)) - n = length (l # ls)"
+          by (metis Suc.prems(2) add_diff_cancel_right' le_add1 length_rev max_def_raw rev.simps(2) right_pad_length_is_correct)
+        then show "drop n (rev (rightPad p (rev ls @ [l]) (Suc padTo\<^sub>p))) = l # ls"
+          by (metis drop_rev length_rev rev.simps(2) rev_swap right_pad_prefix_is_list)
+      qed
+  qed    
+qed
+
+text "additional characters are padding"
+theorem left_pad_adds_padding_character:
+  fixes lst :: "'a list"
+    and p :: "'a"
+    and padTo :: nat
+    and n :: nat
+  assumes "length lst < padTo"
+    and "length lst + n = padTo"
+  shows "\<lbrakk>length lst < padTo; length lst + n = padTo\<rbrakk> 
+      \<Longrightarrow> take n (leftPad p lst padTo) = replicate n p"
+proof(induction lst arbitrary: padTo)
+  case Nil
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case by simp
+  next
+    case (Suc padTo)
+    then show ?case 
+      apply auto 
+      by (simp add: replicate_append_same rightpad_empty_is_replicate)
+  qed
+next
+  case (Cons l ls)
+  then show ?case 
+  proof(induction padTo)
+    case 0
+    then show ?case by simp
+  next
+    case (Suc padTo\<^sub>p)
+    then show ?case 
+      apply auto
+    proof -
+      assume a1: "padTo\<^sub>p = length ls + n"
+      have f2: "length (rightPad p (rev ls @ [l]) (Suc (length ls + n))) = length (l # ls) + n"
+        by (simp add: right_pad_length_is_correct)
+      have f3: "length (rev ls @ [l]) + n = Suc padTo\<^sub>p"
+        using Suc.prems(3) by auto
+      have "length (rev ls @ [l]) < Suc padTo\<^sub>p"
+        using Suc.prems(2) by force
+      then have "drop (length (rev ls @ [l])) (rightPad p (rev ls @ [l]) (Suc padTo\<^sub>p)) = replicate n p"
+        using f3 by (metis (full_types) right_pad_adds_padding_character)
+      then show "take n (rev (rightPad p (rev ls @ [l]) (Suc (length ls + n)))) = replicate n p"
+        using f2 a1 by (simp add: take_rev)
+    qed 
+  qed
+qed
